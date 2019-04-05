325ce443b4b3a497b9ffa010373e8d6c5a07a974
Mitchell Hoffmann <mitchellhoffmann1@gmail.com>
2019-04-05 18:20:13 -0500

    added color feature

---
+++ ColorSegue/ColorSegue/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  ColorSegue
+//
+//  Created by Mitchell John Hoffmann on 4/5/19.
+//  Copyright © 2019 Mitchell John Hoffmann. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+    
+    
+    
+    
+}
